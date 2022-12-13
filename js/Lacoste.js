8c307c3f4777ad56814124213d8a9cd276d9867f
hsy5490 <hsy5490@naver.com>
2022-12-14 03:03:53 +0900

    video

--- js/Lacoste.js
+++ js/Lacoste.js
@@ -29,3 +29,22 @@ tabs.forEach(tab => {
     tab.classList.add('active')
   })
 })
+
+// video play
+const video = $('#l005_video');
+const btn = $('#l005_btn');
+btn.click(function(){
+    /* 초기값 = video(paused) | button(play) */
+    // video가 paused 상태면
+    if(video.get(0).paused){
+        // play() = Video Play
+        $('#l005_video').get(0).play();
+        // button paused 폰트어썸으로 바꿔라
+        $('#l005_btn').html('<i class="fa fa-pause-circle-o" aria-hidden="true"></i>');
+    }else{
+        // pause() = Video Pause
+        $('#l005_video').get(0).pause();
+        // button play 폰트어썸으로 바꿔라
+        $('#l005_btn').html('<i class="fa fa-play-circle-o" aria-hidden="true"></i>');
+    }
+});
